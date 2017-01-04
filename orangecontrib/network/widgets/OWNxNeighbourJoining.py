b30544c5a38c72e785aae7d03a629e60c0fbaae7
Lovro <lovropodgorsek@gmail.com>
2017-01-04 18:13:51 +0100

    Move algorithm in widget

--- orangecontrib/network/widgets/OWNxNeighbourJoining.py
+++ orangecontrib/network/widgets/OWNxNeighbourJoining.py
@@ -5,6 +5,8 @@ from Orange.widgets.settings import Setting
 from Orange.widgets.widget import OWWidget
 from orangecontrib.network import Graph
 
+import numpy as np
+
 
 class _Input:
     DISTANCES = "Distances"
@@ -70,7 +72,56 @@ class OWNeighbourJoining(OWWidget):
 
     def commit(self):
         # todo
-        pass
+        self.join_neighbours(self, _Input)
+
+    def q_value(self, matrix, i, j):
+        return (len(matrix) - 2) * matrix[i][j] - sum(matrix[i]) - sum(matrix[j])
+
+    def minimum_element_index(matrix):
+        return np.unravel_index(matrix.argmin(), matrix.shape)
+
+    def calculate_q(self, distances):
+        n = len(distances)
+        q = np.zeros(shape=(n, n))
+        for i in range(0, n):
+            for j in range(0, n):
+                if i != j:
+                    q[i][j] = self.q_value(distances, i, j)
+        return q
+
+    def calculate_new_distances(self, distances, old_indexes, min_x, min_y):
+        n = len(distances)
+        result = np.zeros(shape=(n - 1, n - 1))
+        for i in range(0, len(old_indexes)):
+            for j in range(0, len(old_indexes)):
+                if i != j:
+                    result[i + 1][j + 1] = distances[old_indexes[i]][old_indexes[j]]
+
+        for i in range(0, len(old_indexes)):
+            d = 0.5 * (distances[min_x][old_indexes[i]] + distances[min_y][old_indexes[i]] - distances[min_x][min_y])
+            result[0][i + 1] = d
+            result[i + 1][0] = d
+        return result
+
+    def join_neighbours(self, distances):
+        print("Initial matrix:\n", distances)
+
+        q_matrix = self.calculate_q(distances)
+        print("Q matrix:\n", q_matrix)
+
+        min_x, min_y = self.minimum_element_index(q_matrix)
+        print("Joining index ", min_x, " and ", min_y)
+        n = len(distances)
+        fu = (0.5 * distances[min_x][min_y] + 1 / (2 * (n - 2)) * (sum(distances[min_x]) - sum(distances[min_y])))
+        gu = distances[min_x][min_y] - fu
+
+        old_indexes = list(range(0, n))
+        old_indexes.remove(min_x)
+        old_indexes.remove(min_y)
+
+        new_matrix = self.calculate_new_distances(distances, old_indexes, min_x, min_y)
+        print("Final matrix: \n", new_matrix)
+        return new_matrix
 
 
 if __name__ == '__main__':
