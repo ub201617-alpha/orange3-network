5a20e19a6256d01ef9a7c472f13d5974ba7b99c4
Jure Prevc <jure.prevc@gmail.com>
2017-01-07 15:04:23 +0100

    Updated neighbour joining with correct Graph output

--- orangecontrib/network/neighbour_joining.py
+++ orangecontrib/network/neighbour_joining.py
@@ -4,82 +4,64 @@ import numpy as np
 class NeighbourJoining:
     @staticmethod
     def _get_minimum_element_index(matrix):
-        return np.unravel_index(matrix.argmin(), matrix.shape)
+        return np.unravel_index(np.nanargmin(matrix), matrix.shape)
 
     def _calculate_q_value(self, i, j):
         matrix = self.distances
-        return (len(matrix) - 2) * matrix[i][j] - sum(matrix[i]) - sum(matrix[j])
+        mask = np.all(np.isnan(matrix), axis=1)
+
+        return (len(matrix) - 2 - sum(mask)) * matrix[i][j] - np.nansum(matrix[i]) - np.nansum(matrix[j])
 
     def _calculate_q_matrix(self):
         n = len(self.distances)
         q = np.zeros(shape=(n, n))
-        for i in range(0, n):
-            for j in range(0, n):
+        for i in set(np.where(~np.isnan(self.distances))[0]):
+            for j in set(np.where(~np.isnan(self.distances))[1]):
                 if i != j:
                     q[i][j] = self._calculate_q_value(i, j)
         return q
 
-    def _calculate_new_distances(self, old_indexes, min_x, min_y):
+    def _calculate_new_distances(self, min_x, min_y):
         distances = self.distances
-        n = len(distances)
-        result = np.zeros(shape=(n - 1, n - 1))
-        for i in range(0, len(old_indexes)):
-            for j in range(0, len(old_indexes)):
-                if i != j:
-                    result[i + 1][j + 1] = distances[old_indexes[i]][old_indexes[j]]
 
-        for i in range(0, len(old_indexes)):
-            d = 0.5 * (distances[min_x][old_indexes[i]] + distances[min_y][old_indexes[i]] - distances[min_x][min_y])
-            result[0][i + 1] = d
-            result[i + 1][0] = d
-        return result
+        joined = np.zeros(distances.shape[0])
+        distances = np.vstack((distances, joined))
+        joined = np.zeros(distances.shape[0])
+        distances = np.column_stack((distances, joined))
+
+        for i in range(0, distances.shape[0] - 1):
+            if i != min_x and i != min_y:
+                d = 0.5 * (distances[min_x][i] + distances[min_y][i] - distances[min_x][min_y])
+                distances[-1][i] = d
+                distances[i][-1] = d
+
+        distances[min_x, :] = np.NaN
+        distances[:, min_x] = np.NaN
+        distances[min_y, :] = np.NaN
+        distances[:, min_y] = np.NaN
+        return distances
 
     def _join_neighbours(self):
         distances = self.distances
 
         q_matrix = self._calculate_q_matrix()
-
         min_x, min_y = self._get_minimum_element_index(q_matrix)
-        n = len(distances)
-        fu = (0.5 * distances[min_x][min_y] + 1 / (2 * (n - 2)) * (sum(distances[min_x]) - sum(distances[min_y])))
-        gu = distances[min_x][min_y] - fu
+        n = np.sum(~np.all(np.isnan(distances), axis=1))  # length without nan values
 
-        old_indexes = list(range(0, n))
-        old_indexes.remove(min_x)
-        old_indexes.remove(min_y)
+        # TODO nansum, etc...
+        d_xy = distances[min_x][min_y]
+        sum_dx = np.nansum(distances[min_x])
+        sum_dy = np.nansum(distances[min_y])
+        fu = (0.5 * d_xy + 1 / (2 * (n - 2)) * (sum_dx - sum_dy))
+        gu = d_xy - fu
 
-        new_matrix = self._calculate_new_distances(old_indexes, min_x, min_y)
-
-        # Add new node that will join min_x and min_y
-        new_node = self.get_new_node_name()
-        self.nodes.append(new_node)
-        # TODO Some weird magic, because we're not keeping track of history. Make better!
-        if min_x:
-            # If min_x is not 0 it means we are adding two leaf nodes (I think)
-            self.last_removed = min_x
-            just_removed = min_x
-        else:
-            # If min_x == 0 , we're adding to existing graph
-            just_removed = self.last_removed
-            self.last_removed = new_node
-
-        # Save new edges and save the calculated distance for them
-        self.edges[(new_node, just_removed)] = fu
-        self.edges[(new_node, min_y + self.removed)] = gu
-
-        # TODO this is some basic (and wrong) way to adjust for missing indexes
-        self.removed += 1
-        return (min_x, min_y), new_matrix
+        self.distances = self._calculate_new_distances(min_x, min_y)
+        return (min_x, min_y), (fu, gu)
 
     def get_final_graph(self):
         """Run self._join_neighbours until we get a full graph
         """
-        while len(self.distances) > 2:
-            (min_x, min_y), self.distances = self._join_neighbours()
-        self.edges[]
-
-        # TODO we need to add the final edge, but can't do so without first implementing some history tracking
-        return self.distances
+        self.__call__()
 
     def get_all_nodes(self):
         return self.nodes
@@ -88,8 +70,8 @@ class NeighbourJoining:
         return self.edges
 
     def __init__(self, data):
-        self.distances = data
-        self.initial_distances = data
+        self.distances = np.array(data)
+        self.initial_distances = np.array(data)
         # TODO probably not the best naming -- if input is DistMatrix, we can use labels for node naming
         # But then again, output won't accept anything other than indexes
         self.nodes = list(range(len(self.distances)))
@@ -104,9 +86,31 @@ class NeighbourJoining:
         return len(self.nodes)
 
     def __call__(self, *args, **kwargs):
-        while len(self.distances) > 2:
-            (min_x, min_y), self.distances = self._join_neighbours()
-            yield (min_x, min_y), self.distances
+        N = len(self.distances)
+        num_of_iterations = N - 3  # by theory
+
+        # init graph
+        max_node = N + num_of_iterations
+        self.nodes = list(range(N))
+        self.nodes.append(max_node)
+        self.edges = {(max_node, i): 0 for i in range(N)}
+
+        for i in range(num_of_iterations):
+            new_node_index = len(self.distances)
+            (j1, j2), (d1, d2) = self._join_neighbours()
+            self.nodes.append(new_node_index)
+            self.edges[(new_node_index, j1)] = d1
+            self.edges[(new_node_index, j2)] = d2
+            self.edges[(max_node, new_node_index)] = 0
+            self.edges.pop((max_node, j1), None)
+            self.edges.pop((max_node, j2), None)
+
+        # last distances calculation
+        (j1, j2), (d1, d2) = self._join_neighbours()
+        self.edges[(max_node, j1)] = d1
+        self.edges[(max_node, j2)] = d2
+        last_idx, _ = self._get_minimum_element_index(self.distances)
+        self.edges[(max_node, last_idx)] = self.distances[max_node][last_idx]
 
 
 if __name__ == '__main__':
@@ -115,9 +119,17 @@ if __name__ == '__main__':
                       [9, 10, 0, 8, 7],
                       [9, 10, 8, 0, 3],
                       [8, 9, 7, 3, 0]]
-    nj = NeighbourJoining(data=test_distances)
-    nj_generator = nj()
 
-    for (x, y), result_distances in nj_generator:
-        print("Joining index ", x, " and ", y)
-        print("New distances: \n", result_distances)
+    test_distances1 = [[0, 5, 4, 7, 6, 8],
+                       [5, 0, 7, 10, 9, 11],
+                       [4, 7, 0, 7, 6, 8],
+                       [7, 10, 7, 0, 5, 9],
+                       [6, 9, 6, 5, 0, 8],
+                       [8, 11, 8, 9, 8, 0]]
+
+    test_distances = np.array(test_distances)
+    nj = NeighbourJoining(data=test_distances)
+    nj.get_final_graph()
+
+    for edge in nj.get_all_edges():
+        print(edge)
--- orangecontrib/network/tests/test_neighbour_joining.py
+++ orangecontrib/network/tests/test_neighbour_joining.py
@@ -5,39 +5,24 @@ from orangecontrib.network.neighbour_joining import NeighbourJoining
 
 class TestNeighbourJoining(unittest.TestCase):
     def setUp(self):
+        # Wikipedia test scenario
+        # https://en.wikipedia.org/wiki/Neighbor_joining
         self.distances = [[0, 5, 9, 9, 8],
                           [5, 0, 10, 10, 9],
                           [9, 10, 0, 8, 7],
                           [9, 10, 8, 0, 3],
                           [8, 9, 7, 3, 0]]
 
+        self.test_edges = {(7, 4): 1.0,
+                           (7, 3): 2.0,
+                           (6, 5): 3.0,
+                           (5, 0): 2.0,
+                           (6, 2): 4.0,
+                           (5, 1): 3.0,
+                           (7, 6): 2.0}
+
     def test_join_neighbours(self):
         nj = NeighbourJoining(self.distances)
-        nj_generator = nj()
-
-        test_data_step_1 = [[0, 7, 7, 6],
-                            [7, 0, 8, 7],
-                            [7, 8, 0, 3],
-                            [6, 7, 3, 0]]
-        test_joined_step1 = (0, 1)
-
-        joined_idx, out_step_1 = next(nj_generator)
-        numpy.testing.assert_array_equal(test_data_step_1, out_step_1)
-        self.assertEqual(joined_idx, test_joined_step1)
-
-        test_data_step_2 = [[0, 4, 3],
-                            [4, 0, 3],
-                            [3, 3, 0]]
-        test_joined_step2 = (0, 1)
-
-        joined_idx, out_step_2 = next(nj_generator)
-        numpy.testing.assert_array_equal(test_data_step_2, out_step_2)
-        self.assertEqual(joined_idx, test_joined_step2)
-
-        test_data_step_3 = [[0, 1],
-                            [1, 0]]
-        test_joined_step3 = (0, 1)
-
-        joined_idx, out_step_3 = next(nj_generator)
-        numpy.testing.assert_array_equal(test_data_step_3, out_step_3)
-        self.assertEqual(joined_idx, test_joined_step3)
+        nj.get_final_graph()
+        edges = nj.get_all_edges()
+        self.assertDictEqual(edges, self.test_edges)
